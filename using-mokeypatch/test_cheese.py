fab6b08a663c5d74e042353fe07c37298704e968
Doug Arcuri <xsurrealx@hotmail.com>
2019-05-01 16:26:19 -0400

    Adding test_def_prefs_change_defaults

--- using-mokeypatch/test_cheese.py
+++ using-mokeypatch/test_cheese.py
@@ -1,4 +1,5 @@
 import cheese
+import copy
 
 
 def test_def_prefs_full():
@@ -24,3 +25,21 @@ def test_def_prefs_change_expanduser(tmpdir, monkeypatch):
     expected = cheese._default_prefs
     actual = cheese.read_cheese_preferences()
     assert expected == actual
+
+
+def test_def_prefs_change_defaults(tmpdir, monkeypatch):
+    fake_home_dir = tmpdir.mkdir('home')
+    monkeypatch.setattr(cheese.os.path, 'expanduser',
+                        (lambda x: x.replace('~', str(fake_home_dir))))
+    cheese.write_default_cheese_preferences()
+    defaults_before = copy.deepcopy(cheese._default_prefs)
+    monkeypatch.setitem(cheese._default_prefs, 'slicing', ['provolone'])
+    monkeypatch.setitem(cheese._default_prefs, 'spreadable', ['brie'])
+    monkeypatch.setitem(cheese._default_prefs, 'salads', ['pepper jack'])
+    defaults_modified = cheese._default_prefs
+
+    cheese.write_default_cheese_preferences()
+
+    actual = cheese.read_cheese_preferences()
+    assert defaults_modified == actual
+    assert defaults_modified != defaults_before
